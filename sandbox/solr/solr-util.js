ab6947599e2c16dfae929995c4495b233a625263
James Clark <this.clark@gmail.com>
2014-03-05 15:23:35 -0600

    demo Solr client is ready for testing

--- sandbox/solr/solr-util.js
+++ sandbox/solr/solr-util.js
@@ -1,7 +1,47 @@
 ﻿var solr = function () { };
-solr.ip = "192.168.1.14";
+solr.ip = "192.168.56.101";
 solr.port = "8983";
-solr.url = "http://192.168.1.14:8983/solr/#/";
+solr.url = "http://" + solr.ip + ":" + solr.port + "/solr/#/";
 solr.toString = function () {
     return solr.url;
 };
+
+var docs = new Array();
+
+function printMediaListItem(doc) {
+	var html = '';
+	html += '<div class="media">';
+	html += '<a class="pull-left" href="#">'; 
+	html += '<img class="media-object" src="' + doc.imageUrl + '" alt="...">';
+	html += '</a>';
+	html += '<div class="media-body">';
+	html += '<h4 class="media-heading">' + doc.title + '</h4>';
+	html += doc.description;
+	html += '</div>';
+	html += '</div>';
+	
+	$('#news-feed-list').append(html);
+}
+
+function init() {
+	var http = 'http://192.168.56.101:8983/solr/newsfeeds_shard1_replica1/select?q=*%3A*&wt=json&indent=true';
+	$.ajax({
+		url: http,
+		dataType: 'jsonp',
+		type: "GET",
+		success: function (response) {
+			docs = response.response.docs;
+			for(var i = 0; i < docs.length; i++) {
+				var doc = new Object;
+				doc.title = docs[i].title[0];
+				doc.description = docs[i].description;
+				doc.imageUrl ="http://www.cpsc.gov" + docs[i].mediaContent;
+				printMediaListItem(doc);
+			}
+		}
+	});
+}
+
+
+
+$(document).ready(init());
