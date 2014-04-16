4ccf3eca57b0678addb47ecb433d198c0d03b45c
jwclark <jwclark@rockhursths.edu>
2014-04-16 15:24:42 -0500

    added data mining pseudo code algorithm to comments

--- sandbox/snippets/gov.digital.search/src/gov/digital/search/Start.java
+++ sandbox/snippets/gov.digital.search/src/gov/digital/search/Start.java
@@ -13,13 +13,17 @@ import java.util.Timer;
 
 public class Start {
 
+	//there are four possible organizations to query
 	private static final String[] orgs = {"CPSC", "FDA", "NHTSA", "USDA"};
+	
+	//this query reveals recalls begin in 1966: http://api.usa.gov/recalls/search.json?start_date=0000-01-01&end_date=1966-01-19
+	private static int startYear = 1966;
 	
 	private static String url = "http://api.usa.gov/recalls/search.json?";
 	private static String query;
 	private static String organization;
-	private static String page;
-	private static String per_page;
+	private static String page; //between 1 and 20
+	private static String per_page; //max 50
 	private static String sort;
 	
 	private static Date start_date;
@@ -30,14 +34,29 @@ public class Start {
 		start_date = new Date(1966, 0, 1);
 		
 		System.out.println(start_date);
-	}
-	
-	private void setUrl() {
 		
-	}
-	
-	private void fastForward() {
-		
+		/* maxResults = 1000
+		 * start = startDate
+		 * end = start + month
+		 * 
+		 * while(start < now + one day) do
+		 * 
+		 * wrap a web request:
+		 *   start a timer
+		 *   on timer complete:
+		 *     get JSON results
+		 *     if(numResults > maxResults)
+		 *       split the date range in half
+		 *         end = timeBetween(start, end)
+		 *         wrap a web request:
+		 *     else
+		 *       process results
+		 *         writeOut(outputFormatWriter)
+		 *         start = end + 1 day
+		 *         end = end + 1 month
+		 *         wrap a web request
+		 *       
+		 */
 	}
 	
 	private void dos() {
@@ -45,18 +64,10 @@ public class Start {
 		for(String org : orgs) {
 			organization = org;
 			
-			//foreach year
-			
-			//foreach month
-			
-			//do this every 2 seconds to avoid rapidly targeting someone else's web server
 			Timer timer = new Timer();
 			TimedRequest request = new TimedRequest();
 			request.setUrl(url);
 			timer.schedule(request, count*2300);
-			
-			
-			//this query reveals first recalls are in 1966: http://api.usa.gov/recalls/search.json?start_date=1966-01-01&end_date=1966-12-31
 			
 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		
