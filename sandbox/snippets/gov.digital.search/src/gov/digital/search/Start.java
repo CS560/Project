bb294b42bb4deaa89cb4bac5c86b17a13bef7622
JamesWClark <jwclark@rockhursths.edu>
2014-04-26 04:05:03 -0500

    added a classified results parser

--- sandbox/snippets/gov.digital.search/src/gov/digital/search/Start.java
+++ sandbox/snippets/gov.digital.search/src/gov/digital/search/Start.java
@@ -1,15 +1,19 @@
 package gov.digital.search;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.json.JSONObject;
-
 public class Start {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
+		String inputFile = "fda-classified-result-01.txt";
+		String outputFile = "fda-classified-parsed-01.tsv";
+		new NBClassificationParser(inputFile, outputFile);
+		
+		
+		/** code to break all the data into respective data files, line-by-line as TSV
 		String fileName = "dataFile.txt";
-		
-		
 		new TSVOrganizationWriter(fileName);
+		*/
 	}
 }
