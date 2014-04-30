dd582d9575f2c1e4a37e3b738a3a9c4963c6eabd
jwclark <jwclark@rockhursths.edu>
2014-04-30 13:57:34 -0500

    working mongo io snippet

--- sandbox/mongodb/src/db/mongo/MongoDatabase.java
+++ sandbox/mongodb/src/db/mongo/MongoDatabase.java
@@ -22,30 +22,63 @@ public class MongoDatabase {
 	
 	private String host;
 	private MongoClient client;
+	private DBCollection userCollection;
 	DB db;
 	
+	/**
+	 * constructor
+	 * @param host
+	 */
 	public MongoDatabase(String host) {
 		this.host = host;
 		connect("test");
 	}
 	
+	/**
+	 * connect to a host, use db, then get user collection
+	 * @param dbName
+	 */
 	public void connect(String dbName) {
 		try {
 			client = new MongoClient(host);
 			db = client.getDB(dbName);
+			userCollection = db.getCollection("users");
 			_Logger.log("connected");
 		} catch (UnknownHostException e) {
 			_Logger.log(e.getMessage());
 		}
-		
 	}
 	
+	/**
+	 * insert a new document
+	 * @param json
+	 */
 	public void insert(String json) {
 		DBObject doc = (DBObject) JSON.parse(json);
+		userCollection.insert(doc);
 	}
+	
+	/**
+	 * update an existing document
+	 */
 	public void update() {
 		
 	}
+	
+	/**
+	 * what's a cursor doc?
+	 */
+	public void doCursorDoc() {
+		DBCursor cursor = userCollection.find();
+		while(cursor.hasNext()) {
+			_Logger.log(cursor.next().toString());
+		}
+	}
+	
+	/**
+	 * get a collection list from a db
+	 * @return
+	 */
 	public Set<String> getCollectionNames() {
 		Set<String> collections =  db.getCollectionNames();
 		return collections;
--- sandbox/mongodb/src/db/mongo/Start.java
+++ sandbox/mongodb/src/db/mongo/Start.java
@@ -6,11 +6,11 @@ public class Start {
 
 
 	public static void main(String[] args) {
+		String j1 = "{'id':'james','email':'this.clark@gmail.com','password':'password'}";
+		
 		String host = "54.186.3.69";
 		MongoDatabase db = new MongoDatabase(host);
-		Set<String> set =  db.getCollectionNames();
-		for(String s : set) {
-			System.out.println(s);
-		}
+		//db.insert(j1);
+		db.doCursorDoc();
 	}
 }
