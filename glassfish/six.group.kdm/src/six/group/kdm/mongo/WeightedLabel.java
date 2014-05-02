ea44f23cc77dbac353458f7703d78c6f92d5c3de
JamesWClark <jwclark@rockhursths.edu>
2014-05-01 23:29:36 -0500

    working on a recommender parser for tiru

---
+++ glassfish/six.group.kdm/src/six/group/kdm/mongo/WeightedLabel.java
@@ -0,0 +1,14 @@
+package six.group.kdm.mongo;
+
+public class WeightedLabel {
+
+	public int weight;
+	public String label;
+	
+	public WeightedLabel(String label) {
+		this.label = label;
+		this.weight = 0;
+	}
+	
+
+}
